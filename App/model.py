34e16408280cf34ac9426e832fff0fb2d88a57b6
Mateo Lopez <m.lopez23@uniandes.edu.co>
2021-09-01 12:57:33 -0500

    Entrega Final – laboratorio 3

--- App/model.py
+++ App/model.py
@@ -54,9 +54,9 @@ def newCatalog():
     catalog['books'] = lt.newList()
     catalog['authors'] = lt.newList('ARRAY_LIST',
                                     cmpfunction=compareauthors)
-    catalog['tags'] = lt.newList('ARRAY_LIST',
+    catalog['tags'] = lt.newList('SINGLE_LINKED',
                                  cmpfunction=comparetagnames)
-    catalog['book_tags'] = lt.newList('ARRAY_LIST')
+    catalog['book_tags'] = lt.newList('SINGLE_LINKED')
 
     return catalog
 
